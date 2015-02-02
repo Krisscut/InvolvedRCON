e85bbc8b4cf2eb29980d95fa778e7b93a1f0c558
Krisscut <krisscut@hotmail.fr>
2015-02-02 01:02:04 +0100

    Deployment v1 ( bug )
    Deployment v1 ( bug )

--- RconInvolved/RconInvolved/Windows/DeploymentWindow.xaml.cs
+++ RconInvolved/RconInvolved/Windows/DeploymentWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Framework.UI.Controls;
+using RconInvolved.DataPersistance;
+using RconInvolved.Utils;
+using System;
 using System.Collections.Generic;
 using System.Deployment.Application;
 using System.Linq;
@@ -21,81 +24,80 @@ namespace RconInvolved.Windows
     public partial class DeploymentWindow
     {
         private UpdateCheckInfo infoUpdate = null;
+        SplashScreenWindow splashScreen = null;
 
-        public DeploymentWindow(UpdateCheckInfo info)
+        public DeploymentWindow(UpdateCheckInfo info, SplashScreenWindow source)
         {
+            Logger.MonitoringLogger.Debug("Deployment window initialized");
             InitializeComponent();
             this.infoUpdate = info;
+            this.splashScreen = source;
+
+            //Set up UI with informations ! ( and upgrade TODO )
+            InstallUpdateSyncWithInfo();
         }
 
         private void InstallUpdateSyncWithInfo()
         {
-            UpdateCheckInfo info = null;
+                Boolean doUpdate = true;
+                ApplicationDeployment ad = Configuration.applicationDeployment;
 
-            if (ApplicationDeployment.IsNetworkDeployed)
-            {
-                ApplicationDeployment ad = ApplicationDeployment.CurrentDeployment;
-                try
+                if (!infoUpdate.IsUpdateRequired)
                 {
-                    info = ad.CheckForDetailedUpdate();
+                    Logger.MonitoringLogger.Debug("Update not required");
+                    //Here, set data information for a non vital version
+                    MessageDialog.ShowAsync(
+                    "Nouvelle version",
+                    "Version non requise mais installée en attendant une gestion plus poussée : nouvelle version : " + infoUpdate.MinimumRequiredVersion.ToString(),
+                    MessageBoxButton.OK,
+                    MessageDialogType.Light,
+                    this);
+                    doUpdate = true;
+                }
+                else
+                {
+                    // Display a message that the app MUST reboot. Display the minimum required version.
+                    /*MessageBox.Show("This application has detected a mandatory update from your current " +
+                        "version to version " + info.MinimumRequiredVersion.ToString() +
+                        ". The application will now install the update and restart.",
+                        "Update Available", MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                        * */
+                    Logger.MonitoringLogger.Debug("Update required");
+                    MessageDialog.ShowAsync(
+                    "Nouvelle version requise",
+                    "Cette nouvelle version est obligatoire, la mise à jour va se faire automatiquement vers la version " + infoUpdate.MinimumRequiredVersion.ToString(),
+                    MessageBoxButton.OK,
+                    MessageDialogType.Light,
+                    this);
+                }
 
-                }
-                catch (DeploymentDownloadException dde)
+                if (doUpdate)
                 {
-                    MessageBox.Show("The new version of the application cannot be downloaded at this time. \n\nPlease check your network connection, or try again later. Error: " + dde.Message);
-                    return;
-                }
-                catch (InvalidDeploymentException ide)
-                {
-                    MessageBox.Show("Cannot check for a new version of the application. The ClickOnce deployment is corrupt. Please redeploy the application and try again. Error: " + ide.Message);
-                    return;
-                }
-                catch (InvalidOperationException ioe)
-                {
-                    MessageBox.Show("This application cannot be updated. It is likely not a ClickOnce application. Error: " + ioe.Message);
-                    return;
-                }
-
-                if (info.UpdateAvailable)
-                {
-                    /*
-                    Boolean doUpdate = true;
-
-                    if (!info.IsUpdateRequired)
+                    try
                     {
-                        DialogResult dr = MessageBox.Show("An update is available. Would you like to update the application now?", "Update Available", MessageBoxButtons.OKCancel);
-                        if (!(DialogResult.OK == dr))
-                        {
-                            doUpdate = false;
-                        }
+                        Logger.MonitoringLogger.Warn("Application update is starting");
+                        ad.Update();
+                        MessageDialog.ShowAsync(
+                            "Installation terminée",
+                            "Installation terminée, redémarrage !",
+                            MessageBoxButton.OK,
+                            MessageDialogType.Light,
+                            this);
+                        Logger.MonitoringLogger.Warn("Application update ended, restarting application !");
+                        System.Windows.Forms.Application.Restart();
                     }
-                    else
+                    catch (DeploymentDownloadException dde)
                     {
-                        // Display a message that the app MUST reboot. Display the minimum required version.
-                        MessageBox.Show("This application has detected a mandatory update from your current " +
-                            "version to version " + info.MinimumRequiredVersion.ToString() +
-                            ". The application will now install the update and restart.",
-                            "Update Available", MessageBoxButtons.OK,
-                            MessageBoxIcon.Information);
+                        MessageBox.Show("Cannot install the latest version of the application. \n\nPlease check your network connection, or try again later. Error: " + dde);
+                        splashScreen.versionChecked = true;
+                        return;
                     }
-
-                    if (doUpdate)
-                    {
-                        try
-                        {
-                            ad.Update();
-                            MessageBox.Show("The application has been upgraded, and will now restart.");
-                            Application.Restart();
-                        }
-                        catch (DeploymentDownloadException dde)
-                        {
-                            MessageBox.Show("Cannot install the latest version of the application. \n\nPlease check your network connection, or try again later. Error: " + dde);
-                            return;
-                        }
-                    }
-                     * */
                 }
-            }
+                
+                //No update : version is checked !
+                splashScreen.versionChecked = true;
+                this.Close();
         }
 
     }
--- RconInvolved/RconInvolved/Windows/SplashScreenWindow.xaml.cs
+++ RconInvolved/RconInvolved/Windows/SplashScreenWindow.xaml.cs
@@ -19,37 +19,48 @@
     /// </summary>
     public partial class SplashScreenWindow
     {
-        private Boolean versionChecked = false;
+        public Boolean versionChecked = false;
+        private Boolean changelogChoice = false;
         private DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        private DispatcherTimer changelogTimer = new DispatcherTimer();
         public Label contentLoading = null;
 
         public SplashScreenWindow()
         {
+            Logger.MonitoringLogger.Debug("Splashscreen window Instanciated");
             InitializeComponent();
             this.contentLoading = this.ActualContent;
 
             dispatcherTimer.Tick += new EventHandler(OnTimerEnded);
             dispatcherTimer.Interval = new TimeSpan(0, 0, 5);
             dispatcherTimer.Start();
+
             //Set application Look
             //TODO load from configFile
             Configuration.app.Theme = Elysium.Theme.Dark;
             Configuration.app.AccentColor = System.Windows.Media.Colors.DarkOrange;
             Configuration.app.ContrastColor = System.Windows.Media.Colors.LightGray;
 
+            //Show splashscreen window
             this.Show();
+
             //Checks if we are in debug or downloaded version
             if (Configuration.applicationDeployment != null) AsyncOperations();
-            else versionChecked = true; 
+            else
+            {
+                Logger.MonitoringLogger.Debug("Application is launched without applicationDeployment");
+                versionChecked = true;
+                changelogChoice = true;
+            }
         }
 
         private async void AsyncOperations()
         {
-            await ChecksForDeployment();
-            await WaitForChangelog();
+            Logger.MonitoringLogger.Debug("Splashscreen window async operations");
+            ChecksForDeployment();
         }
 
-        private async Task ChecksForDeployment()
+        private void ChecksForDeployment()
         {
             contentLoading.Content = "Vérification de version";
             try
@@ -60,33 +71,37 @@
                 try
                 {
                     info = Configuration.applicationDeployment.CheckForDetailedUpdate();
-
                 }
                 catch (DeploymentDownloadException dde)
                 {
                     MessageBox.Show("The new version of the application cannot be downloaded at this time. \n\nPlease check your network connection, or try again later. Error: " + dde.Message);
-                    return;
                 }
                 catch (InvalidDeploymentException ide)
                 {
                     MessageBox.Show("Cannot check for a new version of the application. The ClickOnce deployment is corrupt. Please redeploy the application and try again. Error: " + ide.Message);
-                    return;
                 }
                 catch (InvalidOperationException ioe)
                 {
                     MessageBox.Show("This application cannot be updated. It is likely not a ClickOnce application. Error: " + ioe.Message);
-                    return;
                 }
-                Logger.MonitoringLogger.Info("Info retrieved with check For Detailed Update : " + info.ToString());
+                Logger.MonitoringLogger.Info("Info retrieved with check For Detailed Update : " + info.UpdateAvailable.ToString());
 
+                //Update is available, launch deployment screen !
                 if (info.UpdateAvailable)
                 {
-                    DeploymentWindow deployWindow = new DeploymentWindow(info);
+                    DeploymentWindow deployWindow = new DeploymentWindow(info, this);
                     deployWindow.Show();
+
+                    Logger.MonitoringLogger.Debug("Splashscreen window begin to wait the version to be checked");
+                    this.contentLoading.Content = "En attente du système de mise à jour";
+                    changelogTimer.Tick += new EventHandler(OnTimerChangelogEnded);
+                    changelogTimer.Interval = new TimeSpan(0, 0, 3);
+                    changelogTimer.Start();
                 }
                 else
                 {
                     versionChecked = true;
+                    CheckForChangelog();
                 }
             }
             catch (Exception e)
@@ -97,30 +112,68 @@
             }
         }
 
-        private async Task WaitForChangelog ()
+        private void OnTimerChangelogEnded(object sender, EventArgs e)
         {
-            //Wait for the version to be checked
+            Logger.MonitoringLogger.Debug("Splashscreen is waiting for changelog value to be set");
+            if (!versionChecked) return;
+            CheckForChangelog();
+        }
 
+        private async void CheckForChangelog ()
+        {
+            //First run, ask for the changelog
+            if (Configuration.applicationDeployment.IsFirstRun)
+            {
+                contentLoading.Content = "Affichage changelog ?";
+                await MessageDialog.ShowAsync(
+                    "Nouvelle version !",
+                    "Voulez-vous afficher le changelog de la nouvelle version?",
+                    new[] 
+                        {
+                            new MessageDialogButton()
+                            {
+                                Command = new DelegateCommand(() => ConfirmChangelogView()),
+                                Content = "Oui"
+                            },
+                            new MessageDialogButton()
+                            {
+                                Command = new DelegateCommand(() => NoChangelogView()),
+                                Content = "Non"
+                            }
+                        },
+                    MessageDialogType.Light,
+                    this);
+                NotifyBox.Show(
+                (DrawingImage)this.FindResource("SearchDrawingImage"),
+                "Nouvelle version",
+                "Vous utilisez une nouvelle version de l'application : " + Configuration.applicationDeployment.CurrentVersion.ToString(),
+                false);
+            }
+            else    //Second run or more
+            {
+                changelogChoice = true;
+            }
         }
 
         private void ConfirmChangelogView()
         {
             Process.Start(Configuration.URL_CHANGELOG);
-            versionChecked = true;
+            changelogChoice = true;
         }
         private void NoChangelogView()
         {
             Logger.MonitoringLogger.Info("use have choosen to don't show the changelog.");
-            versionChecked = true;
+            changelogChoice = true;
         }
 
          // Specify what you want to happen when the Elapsed event is raised.
         private void OnTimerEnded(object sender, EventArgs e)
          {
-             Logger.MonitoringLogger.Info("Splashscreen timer, checking all parameters");
+             Logger.MonitoringLogger.Info("Splashscreen timer, versionChecked :" + versionChecked.ToString() + " - changelogChoice :" + changelogChoice.ToString());
              //All verification are done?
              if (!versionChecked) return;
-
+             Logger.MonitoringLogger.Info("Splashscreen timer, checking if Changelog need to be displayed");
+             if (!changelogChoice) return;
 
              contentLoading.Content = "Lancement du RconInvolved";
              dispatcherTimer.Stop();
